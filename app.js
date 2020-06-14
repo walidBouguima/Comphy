041a2788c3a81f7a3c56a3580695242512fac832
walid <walid.bouguima@gmail.com>
2020-06-14 01:22:31 +0100

    destructuring JSON content using map

--- app.js
+++ app.js
@@ -21,7 +21,15 @@ class Products {
        try {
         let result = await fetch('products.json'); 
         let data = await result.json(); 
-        return data;   
+        let products = data.items; 
+        //destructuring JSON content using map
+        products = products.map(item => {
+            const {title,price} = item.fields; 
+            const {id} = item.sys
+            const image = item.fields.image.fields.file.url; 
+            return {title,price,id,image}
+        })
+        return products;   
        } catch (error) {
            console.log(error)
        }
